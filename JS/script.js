c2d28203bdb85300ee1054ef944ace61dad23a98
Jarries-Vodacom <jan.britz@vodacom.co.za>
2021-10-07 12:57:32 +0200

    Uploading all the files

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+function whoissediiey() {
+  document.getElementById("Main").style.fontSize = "25px";
+  document.getElementById("Main").style.color = "black";
+  document.getElementById("Main").style.backgroundColor = "Azure";
+}
+function WhoisWomenVent() {
+  document.getElementById("WV").style.fontSize = "25px";
+  document.getElementById("WV").style.color = "black";
+  document.getElementById("WV").style.backgroundColor = "Azure";
+}
+function display() {
+  document.getElementById('Affirmations').innerHTML = "Today's affirmations".
+  innerHTML=affirmationss;}
+  const affirmationss = ["I am loved", "I am worthy", "I am one of a kind"];
+document.getElementById("Affirmations").innerHTML = affirmationss;
